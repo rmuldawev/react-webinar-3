831afc43b7b2e7948bbc30e4cf94409f8224d1ac
ruslan <rmuldawev@gmail.com>
2023-12-07 03:09:16 +0500

    created pagination and async load function

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -1,4 +1,4 @@
-import {memo, useCallback, useEffect} from 'react';
+import React, { memo, useCallback, useEffect, useState } from "react";
 import Item from "../../components/item";
 import PageLayout from "../../components/page-layout";
 import Head from "../../components/head";
@@ -6,45 +6,100 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import '../main/styles.css'
 
 function Main() {
-
   const store = useStore();
 
-  useEffect(() => {
-    store.actions.catalog.load();
-  }, []);
+  const [pagination, setPagination] = useState({ limit: 10, skip: 0 });
+  const [totalCount, setTotalCount] = useState(0);
 
-  const select = useSelector(state => ({
+  const updatePagination = (newLimit, newSkip) =>
+    setPagination({ limit: newLimit, skip: newSkip });
+
+  useEffect(() => {
+    store.actions.catalog.load(pagination.limit, pagination.skip);
+  }, [pagination]);
+
+  useEffect(() => {
+    setTotalCount(store.getState().catalog.totalCount || 0);
+  }, [store.getState().catalog.totalCount]);
+
+  const select = useSelector((state) => ({
     list: state.catalog.list,
     amount: state.basket.amount,
-    sum: state.basket.sum
+    sum: state.basket.sum,
   }));
-  console.log('select.list',select.list)
 
   const callbacks = {
-    // Добавление в корзину
-    addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
-    // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
-  }
-
-  const renders = {
-    item: useCallback((item) => {
-      return <Item item={item} onAdd={callbacks.addToBasket}/>
-    }, [callbacks.addToBasket]),
+    addToBasket: useCallback(
+      (_id) => store.actions.basket.addToBasket(_id),
+      [store]
+    ),
+    openModalBasket: useCallback(
+      () => store.actions.modals.open("basket"),
+      [store]
+    ),
   };
 
+  const renders = {
+    item: useCallback(
+      (item) => {
+        return <Item item={item} onAdd={callbacks.addToBasket} />;
+      },
+      [callbacks.addToBasket]
+    ),
+  };
+
+  const renderPagination = () => {
+    const totalPages = Math.ceil(totalCount / pagination.limit);
+    const currentPage = Math.ceil((pagination.skip + 1) / pagination.limit);
+  
+    const pages = [];
+  
+    for (let i = 1; i <= totalPages; i++) {
+      const isCurrentPage = i === currentPage;
+  
+      if (
+        i === 1 || 
+        i === totalPages ||
+        (i >= currentPage - 1 && i <= currentPage + 1)
+      ) {
+    
+  
+        pages.push(
+          <button
+            key={i}
+            onClick={() => updatePagination(10, (i - 1) * 10)}
+            className={isCurrentPage ? 'pagination_active_button' :'pagination-button'}
+          >
+            {i}
+          </button>
+        );
+      } else if (i === currentPage - 2 || i === currentPage + 2) {
+        pages.push(<span key={i} style={{color:'#CCCCCC'}}>...</span>);
+      }
+    }
+  
+    return pages;
+  };
+
+  
   return (
     <PageLayout>
-      <Head title='Магазин'/>
-      <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
-                  sum={select.sum}/>
-      <List list={select.list} renderItem={renders.item}/>
-    </PageLayout>
+      <Head title="Магазин" />
+      <BasketTool
+        onOpen={callbacks.openModalBasket}
+        amount={select.amount}
+        sum={select.sum}
+      />
+      <List list={select.list} renderItem={renders.item} />
 
+      <div style={{ display: "flex", justifyContent: "flex-end",marginTop:'22px',marginRight:'21px'}}>
+        {renderPagination()}
+      </div>
+    </PageLayout>
   );
 }
 
 export default memo(Main);
-
--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -14,12 +14,13 @@ class Catalog extends StoreModule {
     }
   }
 
-  async load() {
-    const response = await fetch('/api/v1/articles');
+  async load(limit, skip) {
+    const response = await fetch(`/api/v1/articles?limit=${limit}&skip=${skip}&fields=items(_id, title, price),count`);
     const json = await response.json();
     this.setState({
       ...this.getState(),
-      list: json.result.items
+      list: json.result.items,
+      totalCount: json.result.count,
     }, 'Загружены товары из АПИ');
   }
 }
